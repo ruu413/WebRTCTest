68e293f5049d519916fd8c343ac313be9acce645
kento <asikaga613-kento@yahoo.co.jp>
2021-10-23 23:42:05 +0900

    third commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: {
-        skyway_sender: './skyway_sender.js',
-        skyway_receiver: './skyway_receiver.js',
-        momo_sender: './momo_sender.js',
+    skyway_receiver: "./skyway_receiver.js",
+    momo_sender: "./momo_sender.js",
   },
   output: {
-    filename: '[name]_.js',
-    path: path.resolve(__dirname)
-  }
+    filename: "[name]_.js",
+    path: path.resolve(__dirname),
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      MOMO_WSURL: '"ws://127.0.0.1:8080/ws"',
+      SW_WSURL: '"ws://127.0.0.1:8081"',
+    }),
+  ],
 };
